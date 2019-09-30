47ac215e9ecc527290c9e321db6849c99088413c
Rashmi <poddar.rashmi@gmail.com>
2019-09-30 16:40:23 -0400

    Implemented add functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: [{
+        id: 1,
+        task: 'Complete sprint',
+        completed: false
+      }]
+    }
+  }
+
+  addTodo = newTask => {
+    const newTodo = {
+      id: Date.now(),
+      task: newTask,
+      completed: false
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoList todos={this.state.todos} />
+        <TodoForm
+          addTodo={this.addTodo}
+        />
       </div>
     );
   }
